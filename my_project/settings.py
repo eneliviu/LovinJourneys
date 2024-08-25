c657d8caee55b30f3193c3dff8a4d4a546d3c1e8
Liviu Theodor Ene <eneliviu@gmail.com>
2024-08-25 17:21:22 +0000

    Create superuser

--- my_project/settings.py
+++ my_project/settings.py
@@ -34,12 +34,14 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    # pre-installed apps:
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps:
     'trip_app',
 ]
 
--- trip_app/urls.py
+++ trip_app/urls.py
@@ -3,4 +3,5 @@ from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    # path('dashboard/', journeys_list, name='journeys-list')
 ]
