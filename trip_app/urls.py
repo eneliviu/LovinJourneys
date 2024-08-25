cc8087622615b207ffd8b0b532404996ec7f61e6
Liviu Theodor Ene <eneliviu@gmail.com>
2024-08-25 18:03:13 +0000

    Formatting

--- trip_app/urls.py
+++ trip_app/urls.py
@@ -3,5 +3,5 @@ from .views import HomeView, journey_list
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('dashboard/', journey_list, name='journey-list')
+    path('dashboard/', journey_list, name='journey-list'),
 ]
